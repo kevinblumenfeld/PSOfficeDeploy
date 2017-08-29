47c874c99dd41c4cda0190c6fcad3b65fef382b2
kevinblumenfeld <github@thenext.net>
2017-08-29 18:11:32 -0400

    added logfile to the logfilepath parameter of uodotsourceC2R

--- PSOfficeDeploy/Deploy/uoDotSourceC2R.ps1
+++ PSOfficeDeploy/Deploy/uoDotSourceC2R.ps1
@@ -1,2 +1,3 @@
 . C:\oScripts\Deploy\uninstallC2R\Remove-OfficeClickToRun.ps1
-Remove-OfficeClickToRun
+$LogFile = ($(get-date -Format yyyy-MM-dd_HH-mm-ss) + "-Remove_CLICK2RUN_Office_Installs.csv")
+Remove-OfficeClickToRun -LogFilePath $LogFile
