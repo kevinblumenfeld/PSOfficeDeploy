982a20559423cafa134a6ba94d768daf77cc9304
kevinblumenfeld <github@thenext.net>
2017-08-29 18:24:06 -0400

    specified path for both log files in both uodotsource

--- PSOfficeDeploy/Deploy/uoDotSourceC2R.ps1
+++ PSOfficeDeploy/Deploy/uoDotSourceC2R.ps1
@@ -1,3 +1,3 @@
 . C:\oScripts\Deploy\uninstallC2R\Remove-OfficeClickToRun.ps1
 $LogFile = ($(get-date -Format yyyy-MM-dd_HH-mm-ss) + "-Remove_CLICK2RUN_Office_Installs.csv")
-Remove-OfficeClickToRun -LogFilePath $LogFile
+Remove-OfficeClickToRun -LogFilePath "C:\oScript\" + $LogFile
